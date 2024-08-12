e44f4f0576cee7ec0f369f3456d7692355b47719
prabhav-oodles <168636573+prabhav-oodles@users.noreply.github.com>
2024-08-12 17:23:48 +0530

    Update image-optimization.ts
    Added multiple bucket support

--- bin/image-optimization.ts
+++ bin/image-optimization.ts
@@ -3,9 +3,11 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ImageOptimizationStack } from '../lib/image-optimization-stack';
 
-
 const app = new cdk.App();
-new ImageOptimizationStack(app, 'ImgTransformationStack', {
 
-});
+// ProfileA stack
+new ImageOptimizationStack(app, "ImgTransformationStackOodlesllc", {});
+
+// Dev stack
+new ImageOptimizationStack(app, "ImgTransformationStackPublic", {});
 
